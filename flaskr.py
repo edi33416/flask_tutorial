1f27007ee8575bdcbcc0f800e84197b48637ace2
edi33416 <edi33416@gmail.com>
2015-11-19 00:53:52 +0200

    Add view functions

--- flaskr.py
+++ flaskr.py
@@ -43,6 +43,55 @@ def connect_db():
 
 # end of database related functions
 
+# view functions
+
+# show all the entries stored in the database in descending order, from the
+# most recent one to the oldest one
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title = row[0], text = row[0]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries = entries)
+
+# allow a logged in user to add a new entry to the database
+@app.route('/add', methods = ['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+
+    # Security Note
+    # use question marks when building SQL statements in order to prevent
+    # SQL injection when using string formatting
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                [request.form['title'], request.form['text']])
+    # do not forget to commit the changes
+    g.db.commit()
+
+    flash('New entry was successfully posted')
+
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were successfully logged in')
+            return redirect(url_for('show_entries'))
+
+    return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were successfully logged out')
+    return redirect(url_for('show_entries'))
+
 if __name__ == '__main__':
-    #init_db()
+    init_db()
     app.run()
