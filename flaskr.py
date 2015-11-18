979ab06a1f7cf7ea68f3cd0fa3a10b46e839ca0a
edi33416 <edi33416@gmail.com>
2015-11-19 01:20:23 +0200

    Fix add_entries bug

--- flaskr.py
+++ flaskr.py
@@ -50,7 +50,7 @@ def connect_db():
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title = row[0], text = row[0]) for row in cur.fetchall()]
+    entries = [dict(title = row[0], text = row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries = entries)
 
 # allow a logged in user to add a new entry to the database
